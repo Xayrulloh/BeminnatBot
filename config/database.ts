f756e9c44c28ae7b705a5969de0b00cc420106f8
Abduraxim <toxirmalk@gmail.com>
2024-12-21 17:54:57 +0500

    Dasturda yangilanishlar

--- config/database.ts
+++ config/database.ts
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from 'mongoose'
-import { IAddress, IUser } from '#types/database'
+import { IAddress, ICategory, IOrders, IProducts, IUser } from '#types/database'
 import { env } from '#utils/env'
 import { Color } from '#utils/enums'
 
@@ -52,8 +52,83 @@ const Address = new Schema(
   { versionKey: false },
 )
 
+const Products = new Schema(
+  {
+    id: {
+      required: true,
+      type: Number,
+    },
+
+    name: {
+      required: true,
+      type: String,
+    },
+    description: {
+      required: true,
+      type: String,
+    },
+    image: {
+      required: true,
+      type: String,
+    },
+    price: {
+      required: true,
+      type: Number,
+    },
+    categoryId: {
+      required: true,
+      type: Number,
+      ref: 'Category',
+    },
+  },
+  { versionKey: false },
+)
+
+const Category=new Schema({
+  id:{
+    required: true,
+      type: Number,
+  }
+  ,
+  name:{
+    required: true,
+    type:String,
+  }
+},
+{ versionKey: false },)
+
+const Orders=new Schema({
+  id:{
+    required: true,
+      type: Number,
+  }
+  ,
+  productId:{
+    required:true,
+    type:Number,
+    ref:'Product'
+  },
+  userId:{
+    required:true,
+    type:Number,  
+    ref:'User'
+  }
+  ,
+  status:{
+    required:true,
+    type: Boolean
+  },
+  count:{
+    required:true,
+    type:Number 
+  }
+},
+  { versionKey: false },)
 mongoose.model<IUser>('User', User)
 mongoose.model<IAddress>('Address', Address)
+mongoose.model<ICategory>('Category', Category)
+mongoose.model<IProducts>('Products', Products)
+mongoose.model<IOrders>('Orders', Orders)
 mongoose.set('strictQuery', false)
 
 mongoose
--- types/database.ts
+++ types/database.ts
@@ -14,3 +14,30 @@ export interface IAddress extends Document {
   longitude: number
   userId: number
 }
+
+
+export interface IProducts extends Document {
+  id: number,
+  name: string,
+  description: string
+  image: string
+  price :number,
+  categoryId:number
+}
+
+
+export interface ICategory extends Document {
+  id: number,
+  name: string,
+
+}
+
+
+
+export interface IOrders extends Document {
+  id: number,
+  productId:number
+  userId:number
+  status:boolean,
+  count:number
+}
